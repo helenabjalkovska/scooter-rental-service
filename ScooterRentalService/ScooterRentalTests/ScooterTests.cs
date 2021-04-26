336a998c4e9ee138a5e8bd11dbd2f7622e804f75
Helena Bjalkovska <helena.bjalkovska@gmail.com>
2021-04-26 11:46:34 +0300

    Program set up

---
+++ ScooterRentalService/ScooterRentalTests/ScooterTests.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ScooterRentalTests
+{
+    [TestClass]
+    public class ScooterTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
